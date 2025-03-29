46f7bb906fb66b291082d3abf4b64d1e1560ad62
Risqie <risqienurilman@gmail.com>
2025-03-29 19:52:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 from wordcloud import WordCloud
+import gdown
 
 # Konfigurasi dashboard
 st.set_page_config(page_title="E-Commerce Dashboard", layout="wide")
